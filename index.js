a8ee4ee4b5b603a19f28483b9ce0ab79b400a0b2
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-05-08 10:52:55 -0300

    feat: add mute feature

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
-const SPEED = 8;
+const SPEED = 16;
+let originalSpeed = 1;
+let originalMuted = false;
+let wasAd = false;
 
 function handleAd() {
     const video = document.querySelector('video');
-    const isAd = document.querySelector('.ad-showing');
-
     if (!video) return;
 
+    const adSelectors = [
+        '.ad-showing',
+        '.ad-interrupting',
+        '.ytp-ad-player-overlay',
+        '.ytp-ad-skip-button',
+        '.ytp-ad-skip-button-modern'
+    ];
+
+    const adShowing = document.querySelector(adSelectors.join(', '));
+
+    const skipSelectors = [
+        '.ytp-ad-skip-button',
+        '.ytp-skip-ad-button',
+        '.ytp-ad-skip-button-modern',
+        '.ytp-skip-ad-button-modern',
+        '.ytp-ad-skip-button-container [class*="skip"]',
+        '[class*="ytp-ad-skip-button"]'
+    ];
+    const skipButton = document.querySelector(skipSelectors.join(', '));
+
+    const overlayCloseButton = document.querySelector('.ytp-ad-overlay-close-button');
+
+    const isAd = !!(adShowing || skipButton);
+
     if (isAd) {
+        if (!wasAd) {
+            originalSpeed = video.playbackRate;
+            originalMuted = video.muted;
+            wasAd = true;
+        }
+
         if (video.playbackRate !== SPEED) {
             video.playbackRate = SPEED;
         }
-
+        if (!video.muted) {
+            video.muted = true;
+        }
     } else {
-        if (video.playbackRate !== 1) {
-            video.playbackRate = 1;
+        if (wasAd) {
+            video.playbackRate = originalSpeed;
+            video.muted = originalMuted;
+            wasAd = false;
         }
     }
+
+    // TODO: make this work in the future... I guess...
+    //
+    // if (overlayCloseButton) {
+    //     const rect = overlayCloseButton.getBoundingClientRect();
+    //     if (rect.width > 0 && rect.height > 0) {
+    //         console.log('[AdSkipper] Closing overlay');
+    //         simulateClick(overlayCloseButton);
+    //     }
+    // }
 }
 
-setInterval(handleAd, 300);
+setInterval(handleAd, 200);
